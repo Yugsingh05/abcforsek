e2e5db162960ad49ef3377fe904dffe48fcc412f
Yugsingh05 <yugsingh7622@gmail.com>
2025-08-15 12:36:05 +0530

    feat : remove the dark mode theme l;ogivc and fixed the cs issue int he test block

--- src/components/newpage/ContentRenders/NewTestBlockComponent.tsx
+++ src/components/newpage/ContentRenders/NewTestBlockComponent.tsx
@@ -13,7 +13,7 @@ export default  function NewTestComponent({heading , default_text , icon }: Test
     <div className={clsx('flex flex-col items-center justify-center', 'bg-white p-12')}>
       <h1 className="text-3xl font-bold">{heading}</h1>
       <p className="text-xl">{default_text}</p>
-      <div className="flex flex-row items-center justify-between mt-8">
+      <div className="flex flex-col md:flex-row items-center justify-between mt-8">
         <Image
         alt="icon"
         src={icon}
